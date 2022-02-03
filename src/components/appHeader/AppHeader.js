fa97cae7b04c993150246f2fd74bc1350b9397c0
Yarik <yp.devintus@gmail.com>
2022-02-03 13:33:40 +0200

    React-router-dom 5.3.0 implemented

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,21 +1,23 @@
+import { Link, NavLink } from 'react-router-dom';
+
 import './appHeader.scss';
 
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <a href="#">
+                <Link exact to="/">
                     <span>Marvel</span> information portal
-                </a>
+                </Link>
                 </h1>
             <nav className="app__menu">
                 <ul>
                     <li>
-                        <a href="#">Characters</a>
+                        <NavLink exact to="/" activeClassName="active-page">Characters</NavLink>
                     </li>
                     /
                     <li>
-                        <a href="#">Comics</a>
+                        <NavLink exact to="/comics/" activeClassName="active-page">Comics</NavLink>
                     </li>
                 </ul>
             </nav>
---
+++ src/components/pages/ComicsListPage.js
@@ -0,0 +1,13 @@
+import AppBanner from '../appBanner/AppBanner';
+import ComicsList from '../comicsList/ComicsList';
+
+const ComicsListPage = () => {
+    return (
+        <>
+            <AppBanner/>
+            <ComicsList/>
+        </>
+    )
+}
+
+export default ComicsListPage;
